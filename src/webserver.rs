b32ff0b5c8de721ad95e3faf3deada34d94e3e36
Marco Napetti <marco.napetti@pixartprinting.com>
2018-01-22 08:53:41 +0100

    Modularity improved

--- src/webserver.rs
+++ src/webserver.rs
@@ -1,6 +1,6 @@
 extern crate hyper;
 extern crate futures;
-extern crate serde_json;
+// extern crate serde_json;
 extern crate regex;
 // extern crate ini;
 
@@ -10,9 +10,12 @@ use self::futures::Stream;
 use self::hyper::{Method, StatusCode};
 use self::hyper::server::{Request, Response, Service};
 
-use self::serde_json::Value;
-
 use self::regex::Regex;
+
+// use self::serde_json::Value;
+
+use std::path::Path;
+use std::process::Command;
 
 pub struct WebServer;
 
@@ -35,34 +38,49 @@ impl Service for WebServer {
                     //convert chunks to String
                     let body = String::from_utf8(chunks.to_vec()).unwrap();
 
-                    let mut res = Response::new();
-
-                    // Parse the string of data into serde_json::Value.
-                    let json:Value = match serde_json::from_str(&body){
-                        Ok(val) => val,
-                        Err(err) => {
-                            res.set_status(StatusCode::InternalServerError);
-                            res.set_body(format!("{}", err));
-                            Value::Null
-                        }
-                    };
-
-                    if json != Value::Null {
+//                     // Parse the string of data into serde_json::Value.
+//                     let json:Value = match serde_json::from_str(&body){
+//                         Ok(val) => val,
+//                         Err(err) => {
+//                             res.set_status(StatusCode::InternalServerError);
+//                             res.set_body(format!("{}", err));
+//                             Value::Null
+//                         }
+//                     };
+// 
+//                     if json != Value::Null {
                         lazy_static! {
                             static ref RE: Regex = Regex::new(r"^/Telegram/([^/]+)/([^/]+)").unwrap();
                         }
 
                         match RE.captures(&path) {
                             Some(matches) => {
-                                res.set_body(format!("class: {}\ncode: {}", &matches[1], &matches[2]));
+                                //delegate to another executable bot's execution
+                                //this improves modularity
+                                let path = format!("./bots/{}", &matches[1]);
+                                //it doesn't seem to exists an "is_executable" method
+                                if Path::new(&path).exists() {
+                                    let output = Command::new(path).arg(&matches[2]).arg(body).output().unwrap();
+                                    if output.status.success() {
+                                        Response::new()
+                                            .with_status(StatusCode::Ok)
+                                            .with_body(output.stdout)
+                                    }
+                                    else {
+                                        Response::new()
+                                            .with_status(StatusCode::InternalServerError)
+                                            .with_body(output.stderr)
+                                    }
+                                }
+                                else {
+                                    Response::new().with_status(StatusCode::NotFound)
+                                }
                             },
                             None => {
-                                res.set_status(StatusCode::NotFound);
+                                Response::new().with_status(StatusCode::NotFound)
                             }
                         }
-                    }
-
-                    res
+//                     }
                 }))
             },
             _ => {
