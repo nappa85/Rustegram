fb3c48d537d19cd16effa3ea605bdcfeda9045e0
Marco Napetti <marco.napetti@pixartprinting.com>
2018-02-12 17:18:56 +0100

    Plugin registry

--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -2,7 +2,7 @@ extern crate hyper;
 extern crate futures;
 extern crate regex;
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use self::futures::future::Future;
 use self::futures::Stream;
@@ -12,14 +12,12 @@ use self::hyper::server::{Request, Response, Service};
 
 use self::regex::Regex;
 
-// use std::path::Path;
-// use std::process::Command;
-
 mod registry;
 
-// lazy_static! {
-    static plugin_registry: Arc<registry::PluginRegistry<'static>> = Arc::new(registry::PluginRegistry::new());
-// }
+//singleton
+lazy_static! {
+    static ref REGISTRY: Arc<Mutex<registry::PluginRegistry>> = Arc::new(Mutex::new(registry::PluginRegistry::new()));
+}
 
 pub struct WebServer;
 
@@ -35,53 +33,34 @@ impl Service for WebServer {
     fn call(&self, req: Request) -> Self::Future {
         match req.method() {
             &Method::Post => {
-                let path = req.path().to_owned();
+                lazy_static! {
+                    static ref RE: Regex = Regex::new(r"^/Telegram/([^/]+)/([^/]+)").unwrap();
+                }
 
-                //concat every request's body chunk
-                Box::new(req.body().concat2().map(move |chunks| {
-                    //convert chunks to String
-                    let body = String::from_utf8(chunks.to_vec()).unwrap();
+                match RE.captures(req.path()) {
+                    //concat every request's body chunk
+                    Some(matches) => Box::new(req.body().concat2().map(move |chunks| {
+                        //convert chunks to String
+                        let body = String::from_utf8(chunks.to_vec()).unwrap();
 
-                    lazy_static! {
-                        static ref RE: Regex = Regex::new(r"^/Telegram/([^/]+)/([^/]+)").unwrap();
-                    }
-
-                    match RE.captures(&path) {
-                        Some(matches) => {
-                            //load bot library
-                            //this improves modularity
-                            match plugin_registry.load_plugin(&matches[1]) {
-                                Ok(()) => {
-                                        match plugin_registry.run(&matches[1], &matches[2], body) {
-                                            Ok(out) => Response::new().with_status(StatusCode::Ok).with_body(out),
-                                            Err(e) => Response::new().with_status(StatusCode::InternalServerError).with_body(e),
-                                        }
-                                    },
-                                Err(e) => Response::new().with_status(StatusCode::InternalServerError).with_body(e),
-                            }
-//                             //it doesn't seem to exists an "is_executable" method
-//                             if Path::new(&path).exists() {
-//                                 let output = Command::new(path).arg(&matches[2]).arg(body).output().unwrap();
-//                                 if output.status.success() {
-//                                     Response::new()
-//                                         .with_status(StatusCode::Ok)
-//                                         .with_body(output.stdout)
-//                                 }
-//                                 else {
-//                                     Response::new()
-//                                         .with_status(StatusCode::InternalServerError)
-//                                         .with_body(output.stderr)
-//                                 }
-//                             }
-//                             else {
-//                                 Response::new().with_status(StatusCode::NotFound)
-//                             }
-                        },
-                        None => {
-                            Response::new().with_status(StatusCode::NotFound)
+                        //load bot library
+                        //this improves modularity
+                        let temp = REGISTRY.clone();
+                        let mut plugin_registry = temp.lock().expect("Unable to lock plugin registry");
+                        match plugin_registry.load_plugin(matches.get(1).unwrap().as_str()) {
+                            Ok(()) => {
+                                    match plugin_registry.run(&matches[1], &matches[2], body) {
+                                        Ok(out) => Response::new().with_status(StatusCode::Ok).with_body(out),
+                                        Err(e) => Response::new().with_status(StatusCode::InternalServerError).with_body(e),
+                                    }
+                                },
+                            Err(e) => Response::new().with_status(StatusCode::InternalServerError).with_body(format!("{}", e)),
                         }
-                    }
-                }))
+                    })),
+                    None => Box::new(futures::future::ok(
+                        Response::new().with_status(StatusCode::NotFound)
+                    ))
+                }
             },
             _ => {
                 Box::new(futures::future::ok(
--- src/webserver/registry.rs
+++ src/webserver/registry.rs
@@ -5,39 +5,79 @@ use std::sync::Arc;
 
 use self::dynamic_reload::{DynamicReload, Error, Search, Lib, UpdateState, Symbol, PlatformName};
 
-pub struct PluginRegistry<'a> {
-    handler: DynamicReload<'a>,
-    libs: HashMap<&'a str, Symbol<'a, extern "C" fn(secret: &str, body: String) -> Result<String, String>>>,//TODO
+struct Plugin {
+    callable: bool,
+    lib: Arc<Lib>,
+//     fun: Symbol<'a, extern "C" fn(secret: &str, body: String) -> Result<String, String>>,
 }
 
-impl<'a> PluginRegistry<'a> {
-    pub fn new() -> PluginRegistry<'static> {
+impl Plugin {
+    pub fn new(plug: &Arc<Lib>) -> Plugin {
+        let temp = plug.clone();
+        Plugin {
+            callable: true,
+            lib: temp,
+//             fun: unsafe {
+//                 temp.lib.get(b"init_bot\0").unwrap()
+//             }
+        }
+    }
+
+    pub fn reload_callback(&mut self, state: UpdateState, plug: Option<&Arc<Lib>>) {
+        match state {
+            UpdateState::Before => {
+                self.callable = false;
+            },
+            UpdateState::After => {
+                self.lib = plug.unwrap().clone();
+//                 self.fun = unsafe {
+//                     self.lib.lib.get(b"init_bot\0").unwrap()
+//                 };
+                self.callable = true;
+            },
+            UpdateState::ReloadFailed(_) => println!("Failed to reload"),
+        }
+    }
+
+    pub fn run(&self, secret: &str, body: String) -> Result<String, String> {
+//         let f = self.fun;
+        // In a real program you want to cache the symbol and not do it every time if your
+        // application is performance critical
+        let f: Symbol<extern "C" fn(secret: &str, body: String) -> Result<String, String>> = unsafe {
+            self.lib.lib.get(b"init_bot\0").unwrap()
+        };
+        f(secret, body)
+    }
+}
+
+pub struct PluginRegistry {
+    handler: DynamicReload<'static>,
+    libs: HashMap<&'static str, Plugin>,
+}
+
+impl PluginRegistry {
+    pub fn new() -> PluginRegistry {
         PluginRegistry {
             handler: DynamicReload::new(Some(vec!["bots"]), Some("tmp"), Search::Default),
             libs: HashMap::new(),
         }
     }
 
-    pub fn load_plugin(&mut self, lib: &str) -> Result<(), Error> {
+    pub fn load_plugin(&mut self, lib: &'static str) -> Result<(), Error> {
         if !self.libs.contains_key(lib) {
-            match self.handler.add_library(lib, PlatformName::Yes) {
-                Ok(plugin) => self.libs.insert(lib, plugin.get(b"shared_fun\0").unwrap()),
-                Err(e) => Err(e),
-            }
+            let plug = self.handler.add_library(lib, PlatformName::Yes)?;
+            self.libs.insert(lib, Plugin::new(&plug));
         }
         else {
-            self.handler.update(Self::reload_callback, self);
+            let mut plugin = self.libs.get_mut(lib).unwrap();
+            self.handler.update(Plugin::reload_callback, &mut plugin);
         }
 
         Ok(())
     }
 
-    pub fn reload_callback(&mut self, state: UpdateState, lib: Option<&Arc<Lib>>) {
-        //do we really care?
-    }
-
     pub fn run(&self, lib: &str, secret: &str, body: String) -> Result<String, String> {
-        let fun = self.libs.get(lib).unwrap();
-        fun(secret, body)
+        let plugin = self.libs.get(lib).unwrap();
+        plugin.run(secret, body)
     }
 }
